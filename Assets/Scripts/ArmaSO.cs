4587f210a426b63aa231bd8ba877291811b44a66
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-11-06 16:23:34 +0100

    dasdas

---
+++ Assets/Scripts/ArmaSO.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Arma")]
+public class ArmaSO : ScriptableObject
+{
+    //DATOS
+    public int balasCarador;
+    public int balasBolsas;
+    public float cadenciaAtaque;//1.5s puedes atacar
+    public float distanciaAtaque;
+    public float danhoAtaque;
+
+}
