c3b47a9e814eef458befc00c1f6ebaf06edb9388
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-11-06 17:34:23 +0100

    iohui

--- Assets/Scripts/ArmaManual.cs
+++ Assets/Scripts/ArmaManual.cs
@@ -6,7 +6,7 @@ public class ArmaManual : MonoBehaviour
 {
     [SerializeField] private ArmaSO misDatos;
     [SerializeField] private LayerMask queEsDanhable;//va a atravesar todo objeto a los que si sea dañable como un zombie detras del arbol, el arbol no es dañable pero el zombie si entonces pasa del arbol y daña al zombie aunque este detras
-
+    [SerializeField] private ParticleSystem system;
 
     private Camera cam;
     // Start is called before the first frame update
@@ -18,8 +18,9 @@ public class ArmaManual : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButtonDown(1))
+        if(Input.GetMouseButtonDown(0))
         {
+            system.Play(); //Ejecuto el sistema de Particulas
             if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hitInfo, misDatos.distanciaAtaque))
             {
                 Debug.Log(hitInfo.transform.name);//muestro el nombre de a quien he impactado
