3e2fa03087c14840230042d832046a979bf148f2
Carlosa-1302 <carlosandresinfo13@gmail.com>
2024-12-11 17:50:38 +0100

    dasd

--- Assets/Scripts/ISOMETRICA/SistemaDialogo.cs
+++ Assets/Scripts/ISOMETRICA/SistemaDialogo.cs
@@ -1,15 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class SistemaDialogo : MonoBehaviour
 {
+    [SerializeField] private GameObject marcoDialogo; //Marco a habilitar/deshabilitar
+
+    [SerializeField] private TMP_Text textoDialogo; // El texto donde se veran reflejados los dialogos
+
+    public static SistemaDialogo sistema;
+
+
+    private bool escribiendo;
+    private int indiceFraseActual = 0;
+
+    private DialogoSO dialogoActual; //Para saber en todo momento cual es el dialogo con el que estamos trabajando
+
+
+
+
+
+
+
+
     //PRATÓN SINGLE-TON
     //1. Solo existe una unica instancia de SistemaDialogo
     //2. Es accesible DESDE CUALQUIER PUNTO del programa.
 
     //public static SistemaDialogo trono;
-    public static SistemaDialogo sistema;
 
 
     //Awake se ejecuta antes del Start() idependientemente de que 
@@ -39,12 +58,42 @@ public class SistemaDialogo : MonoBehaviour
     }
     public void IniciarDialogo(DialogoSO dialogo)
     {
-
+        //El dialogo actual que tenemos que tratar es el que me pasan por parámetro.
+        dialogoActual = dialogo;
+        marcoDialogo.SetActive(true);
+        StartCoroutine(CompletarFrase());
     }
-
-    // Update is called once per frame
-    void Update()
+    //Sirve para escribir frase letra por letra
+    private void EscribirFrase()
     {
-        
+
     }
+    //Sirver para autocompletar la frase
+    private IEnumerator CompletarFrase()
+    {
+        //Limpio el texto.
+        textoDialogo.text =string.Empty;
+
+        //Desmenuzo la frase actual por caracteres por separado.
+        char[] fraseEnLetras = dialogoActual.frases[indiceFraseActual].ToCharArray();
+
+        foreach(char letra in fraseEnLetras)
+        {
+            //1.Incluir la letra en el texto
+            textoDialogo.text += letra;
+            //2. Esperar
+            yield return new WaitForSeconds(0.02f);
+
+        }
+    }
+    private void SiguienteFrase()
+    {
+
+    }
+    private void FinalizarDialogo()
+    {
+
+    }
+
+    
 }
